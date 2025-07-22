6f35e83ca96a6789ae5d7a0d886064714a606c7b
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-07-23 00:08:51 +0100

    ✨ (controller/user.go, router/user.go): add support for rendering login page using HTML template

--- adapter/http/controller/user.go
+++ adapter/http/controller/user.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"errors"
+	"html/template"
 	"net/http"
 	"pharmacy/httperror"
 	"pharmacy/model"
@@ -10,11 +11,12 @@ import (
 )
 
 type userController struct {
-	service service.UserService
+	service  service.UserService
+	template *template.Template
 }
 
-func NewUserController(svc service.UserService) *userController {
-	return &userController{service: svc}
+func NewUserController(svc service.UserService, tmpl *template.Template) *userController {
+	return &userController{service: svc, template: tmpl}
 }
 
 func (c *userController) CreateUserAccount(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +26,7 @@ func (c *userController) CreateUserAccount(w http.ResponseWriter, r *http.Reques
 		httperror.BadRequest("invalid json", err).JSONRespond(w)
 		return
 	}
-	
+
 	err := c.service.CreateUserAccount(r.Context(), u)
 	if err != nil {
 		var httperr *httperror.HTTPError
@@ -34,6 +36,14 @@ func (c *userController) CreateUserAccount(w http.ResponseWriter, r *http.Reques
 		}
 		json.NewEncoder(w).Encode(err)
 	}
-	
+
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func (c *userController) GetLoginPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := c.template.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		http.Error(w, "render error", http.StatusInternalServerError)
+	}
 }
--- adapter/http/router/user.go
+++ adapter/http/router/user.go
@@ -1,15 +1,18 @@
 package router
 
 import (
+	"html/template"
 	"net/http"
 	"pharmacy/adapter/http/controller"
 	"pharmacy/service"
 )
 
-func InitUserRouter(svc service.UserService) http.Handler {
-	userController := controller.NewUserController(svc)
+func InitUserRouter(svc service.UserService, tmpl *template.Template) http.Handler {
+	userController := controller.NewUserController(svc, tmpl)
 	userMux := http.NewServeMux()
 	
 	userMux.HandleFunc(http.MethodPost + " /register", userController.CreateUserAccount)
+	userMux.HandleFunc(http.MethodGet + " /login", userController.GetLoginPage)
+	
 	return http.StripPrefix("/user", userMux)
 }
