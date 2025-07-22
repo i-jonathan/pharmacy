44666d9723a8e65fba920e93119ea5455567625e
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-07-22 20:40:18 +0100

    📅 (db/migrations/000001_create_users_table.up.sql): add created_at column to users table with a default value of the current timestamp

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id integer generated always as identity,
     username varchar(50) unique not null,
-    password text not null
+    password text not null,
+    created_at timestamp default current_timestamp
 )
