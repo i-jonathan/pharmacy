f68cb7e7713c840d38e92685489aa15f3357c918
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-07-25 21:48:36 +0100

    ✨ (model/domain.go, model/product.go): add struct tags for database column mapping to improve ORM compatibility

--- model/product.go
+++ model/product.go
@@ -4,15 +4,15 @@ import "fmt"
 
 type Product struct {
 	baseModel
-	Name           string
-	BarCode        *string
-	CategoryID     int
-	Category       Category
-	ReorderLevel   int
-	Manufacturer   *string
-	CostPriceKobo  int
-	DefaultPriceID int
-	DefaultPrice   ProductPrice
+	Name           string       `db:"name"`
+	Barcode        *string      `db:"barcode"`
+	CategoryID     int          `db:"category_id"`
+	ReorderLevel   int          `db:"reorder_level"`
+	Manufacturer   *string      `db:"manufacturer"`
+	CostPriceKobo  int          `db:"cost_price"`
+	DefaultPriceID int          `db:"default_price_id"`
+	Category       Category     `db:"category"`
+	DefaultPrice   ProductPrice `db:"default_price"`
 	PriceOptions   []ProductPrice
 }
 
@@ -30,13 +30,13 @@ func (p ProductPrice) PriceString() string {
 }
 
 func (p Product) CostPriceString() string {
-    return fmt.Sprintf("₦%.2f", float64(p.CostPriceKobo)/100)
+	return fmt.Sprintf("₦%.2f", float64(p.CostPriceKobo)/100)
 }
 
 func (p Product) DefaultSellingPriceKobo() int {
-    return p.DefaultPrice.SellingPriceKobo
+	return p.DefaultPrice.SellingPriceKobo
 }
 
 func (p Product) DefaultSellingPriceString() string {
-    return p.DefaultPrice.PriceString()
+	return p.DefaultPrice.PriceString()
 }
