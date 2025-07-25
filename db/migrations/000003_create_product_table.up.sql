3b5526459c8fa81859b1a34ce15131f25e7a3509
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-07-25 21:47:52 +0100

    🚀 (db/migrations/000003_create_product_table.up.sql): remove unused column 'unit' from product table to streamline database schema

--- db/migrations/000003_create_product_table.up.sql
+++ db/migrations/000003_create_product_table.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS product (
     id integer generated always as identity unique,
     name varchar(255) not null,
     barcode varchar(255),
-    unit varchar(50),
     category_id integer references category(id) not null,
     reorder_level integer default 5,
     manufacturer varchar(255),
