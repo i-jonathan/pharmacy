e50389a152cc3812600a847359e25ec5738649e1
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-07-27 23:36:17 +0100

    ✨ (db/migrations/000001_create_users_table.up.sql): add unique constraint to id field in users table 🚀 (db/migrations/000007_product_batch_table.up.sql): create new product_batch table with necessary fields and constraints ✅ (db/migrations/000007_product_batch_table.down.sql): add down migration for product_batch table

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id integer generated always as identity,
+    id integer generated always as identity unique,
     username varchar(50) unique not null,
     password text not null,
     created_at timestamp default current_timestamp
