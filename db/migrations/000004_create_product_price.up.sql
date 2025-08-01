0225e3d60c7c9f4eff0cd86ced9783ec747193bd
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-08-01 23:52:12 +0100

    ♻️ (db/migrations/000004_create_product_price.up.sql): remove unused column 'is_default' and related unique index to simplify table structure

--- db/migrations/000004_create_product_price.up.sql
+++ db/migrations/000004_create_product_price.up.sql
@@ -3,10 +3,5 @@ CREATE TABLE IF NOT EXISTS product_price (
     product_id integer not null references product(id) on delete CASCADE,
     quantity_per_unit integer not null,
     selling_price integer not null,
-    is_default boolean default false,
     created_at timestamp default current_timestamp
 );
-
-CREATE UNIQUE INDEX one_default_price_per_product
-ON product_price(product_id)
-WHERE is_default = true;
