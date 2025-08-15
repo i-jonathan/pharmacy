cd58670eed37442318cf294da0038937508088ea
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-08-15 19:31:54 +0100

    🔧 (db/migrations/000009_create_stock_movement_table.up.sql): update migration script to ensure movement_type column is not nullable

--- db/migrations/000009_create_stock_movement_table.up.sql
+++ db/migrations/000009_create_stock_movement_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS stock_movement (
     id integer generated always as identity unique,
     product_id integer not null references product(id) on delete CASCADE,
     batch_id integer not null references product_batch(id) on delete CASCADE,
-    movement_type varchar(10) not null,
+    movement_type varchar not null,
     quantity integer not null,
     created_at timestamp default current_timestamp
 )
