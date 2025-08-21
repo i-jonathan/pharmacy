8ddd3200a982a640e44e1094638571cc65905a8f
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-08-21 23:43:36 +0100

    ✨ (model/sales.go): add new Sale model with associated SaleItem and SalePayment models to represent sales data 💡 (model/sales.go): add GenerateReceiptNumber method to automatically generate a unique receipt number for each sale

---
+++ model/sales.go
@@ -0,0 +1,47 @@
+package model
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Sale struct {
+	baseModel
+	ReceiptNumber string `db:"receipt_number"`
+	Status        string `db:"status"`
+	CashierID     int    `db:"cashier_id"`
+	Subtotal      int    `db:"subtotal"`
+	Discount      int    `db:"discount"`
+	Total         int    `db:"total"`
+
+	SaleItems []SaleItem
+	Payments  []SalePayment
+}
+
+type SaleItem struct {
+	baseModel
+	SaleID     int `db:"sale_id"`
+	ProductID  int `db:"product_id"`
+	Quantity   int `db:"quantity"`
+	UnitPrice  int `db:"unit_price"`
+	Discount   int `db:"discount"`
+	TotalPrice int `db:"total_price"`
+
+	Product Product
+}
+
+type SalePayment struct {
+	baseModel
+	SaleID        int    `db:"sale_id"`
+	Amount        int    `db:"amount"`
+	PaymentMethod string `db:"payment_method"`
+}
+
+func (s *Sale) GenerateReceiptNumber() {
+	start := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	days := int(time.Since(start).Hours() / 24)
+	random := rand.Intn(9000) + 1000
+
+	s.ReceiptNumber = fmt.Sprintf("%d%04d", days, random)
+}
