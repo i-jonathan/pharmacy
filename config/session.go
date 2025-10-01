30b5f5026e050d582b0e35501fdf9b097c5e2dcb
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-10-01 21:38:18 +0100

    Symbol(clack:cancel)

--- config/session.go
+++ config/session.go
@@ -1,13 +1,22 @@
 package config
 
-import "github.com/gorilla/sessions"
+import (
+	"time"
+
+	"github.com/gorilla/sessions"
+)
 
 var SessionStore = func() *sessions.CookieStore {
+	now := time.Now()
+	expiry := time.Date(now.Year(), now.Month(), now.Day(), 2, 0, 0, 0, now.Location())
+	if !now.Before(expiry) {
+		expiry = expiry.Add(24 * time.Hour)
+	}
 	var s *sessions.CookieStore
 	s = sessions.NewCookieStore([]byte(Conf.SessionSecret))
 	s.Options = &sessions.Options{
 		Path:     "/",
-		MaxAge:   18 * 60 * 60,
+		MaxAge:   int(expiry.Sub(now).Seconds()),
 		HttpOnly: true,
 		SameSite: 1,
 		// Secure: true, // enable if HTTPS
