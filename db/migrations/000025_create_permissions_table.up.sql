3205929dbc7b921d24e9e3514cddfa64ffdc1c5f
Jonathan Farinloye <farinloyejonathan@gmail.com>
2025-12-15 15:32:20 +0100

    📅 (db/migrations/*.up.sql): add created_at timestamp to all tables for better tracking of when each record was added

--- db/migrations/000025_create_permissions_table.up.sql
+++ db/migrations/000025_create_permissions_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE permissions (
    id integer generated always as identity unique,
    resource varchar(50) not null,
    action varchar(50) not null,
+   created_at timestamptz default now(),
    UNIQUE(resource, action)
 )
