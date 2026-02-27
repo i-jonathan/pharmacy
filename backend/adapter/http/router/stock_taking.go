f76f122c8a7ffc648de15e7b0cdb4d7522053f76
Jonathan Farinloye <farinloyejonathan@gmail.com>
2026-02-27 20:21:34 +0100

    ✨ (backend/adapter/http/router/stock_taking.go): add POST endpoint for completing stock taking and update permissions constant 🚀 (backend/internal/constant/permissions.go): add new permission key for completing stock taking

--- backend/adapter/http/router/stock_taking.go
+++ backend/adapter/http/router/stock_taking.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"pharmacy/adapter/http/controller"
 	"pharmacy/adapter/http/middleware"
+	"pharmacy/internal/constant"
 	"pharmacy/service"
 )
 
@@ -17,6 +18,13 @@ func InitStockTakingRouter(svc service.StockTakingService, tmpl *template.Templa
 	stockTakingMux.Handle(http.MethodGet+" /api/{id}", middleware.AddPermissionsToContext(
 		http.HandlerFunc(stockTakingController.FetchStockTaking),
 	))
+	stockTakingMux.Handle(
+		http.MethodPost+" /api/{id}", middleware.RequirePermissions(
+			constant.RequireAllPermissions, constant.CompleteStockTakingPermissionKey,
+		)(
+			http.HandlerFunc(stockTakingController.CompleteStockTaking),
+		),
+	)
 	stockTakingMux.HandleFunc(http.MethodGet+" /api/{id}/items", stockTakingController.FetchStockTakingItems)
 	stockTakingMux.HandleFunc(http.MethodPost+" /api/{id}/item/{product_id}", stockTakingController.UpdateStockTakingItemCount)
 
--- backend/internal/constant/permissions.go
+++ backend/internal/constant/permissions.go
@@ -1,3 +1,4 @@
 package constant
 
 const ViewSalesTotalPermissionKey = "salestotal:view"
+const CompleteStockTakingPermissionKey = "stock:complete"
