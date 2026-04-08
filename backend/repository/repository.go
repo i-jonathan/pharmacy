bf0bc459ab953b932d0ab3b345c7714e225b896a
Jonathan Farinloye <farinloyejonathan@gmail.com>
2026-04-08 23:55:43 +0100

    ✨ (inventory.go): add function to update product current expiry after receiving new batches 🔧 (repository.go): add method signature for updating product current expiry after receiving 📝 (query.go): add SQL query for updating product current expiry after receiving 🚀 (service/inventory.go): call the new function in ReceiveProductSupply to update product expiry dates

--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -37,6 +37,7 @@ type InventoryRepository interface {
 	BulkCreateStockMovementTx(ctx context.Context, tx *sqlx.Tx, stockMovements []model.StockMovement) error
 	BulkUpdateProductPricesTx(ctx context.Context, tx *sqlx.Tx, updateValues []map[string]any) error
 	BulkFetchProductByIDsTx(ctx context.Context, tx *sqlx.Tx, productIDs []int) ([]model.Product, error)
+	UpdateProductCurrentExpiryAfterReceiving(ctx context.Context, productID int, expiryDate time.Time) error
 	FetchInventoryItems(ctx context.Context) ([]model.InventoryItem, error)
 	FetchPriceByID(ctx context.Context, priceID int) (model.ProductPrice, error)
 	FetchProductByIDWithPrices(ctx context.Context, id int) (model.Product, error)
